38107f9717a2899907366b051dd81ae1c991de1e
polaris <jb.kim@eastarjet.com>
2026-04-07 16:21:02 +0900

    🔧 Chore: tailwind 설정 추가

--- design-system/lib/utils.ts
+++ design-system/lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
+export const tw = (strings: TemplateStringsArray, ...values: (string | number)[]): string => String.raw({ raw: strings }, ...values);
+
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
